26ebea9016680d623d636139216f2745684cda84
Md Alamgir Hossen <alamgir6934@gmail.com>
2022-10-26 01:50:15 +0600

    fakedata-loaded

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -6,9 +6,14 @@ const Login = () => {
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
+                    <div className="text-center lg:text-center md:flex-row-reverse">
                         <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+
+                        <div className='md:flex-row-reverse'>
+                            <button className="btn btn-primary">Log in with google</button>
+                            <button className="btn btn-primary">Log in with Github.</button>
+                        </div>
+
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
@@ -24,7 +29,7 @@ const Login = () => {
                                 </label>
                                 <input type="text" placeholder="password" className="input input-bordered" />
                                 <label className="label">
-                                    <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
+                                    <Link to='/register' className="label-text-alt link link-hover">Forgot password?</Link>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
