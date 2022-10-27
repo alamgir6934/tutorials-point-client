9198cea18db8c7191f355c35ee2df8f3ebb67312
Md Alamgir Hossen <alamgir6934@gmail.com>
2022-10-27 16:30:45 +0600

    environment-file-added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDuvovSDuro-MAZXEqvUypIL4IJyJGoDnw",
-    authDomain: "tutorials-points-a4a41.firebaseapp.com",
-    projectId: "tutorials-points-a4a41",
-    storageBucket: "tutorials-points-a4a41.appspot.com",
-    messagingSenderId: "278589697900",
-    appId: "1:278589697900:web:d9b7118219c9ffa29d8d49"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
